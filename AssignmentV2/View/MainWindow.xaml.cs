91871ed801da6b2f33490bafb4e7385ed4295e6b
Ilya Post <im-popow13@mail.ru>
2024-11-11 14:54:36 +0500

    Добавьте файлы проекта.

---
+++ AssignmentV2/View/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace AssignmentV2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MinimizedButton_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.MainWindow.WindowState = WindowState.Minimized;
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.MainWindow.Close();
+		}
+	}
+}
---
+++ AssignmentV2/View/UserControls/AuthorizationUserControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AssignmentV2.View.UserControls
+{
+	/// <summary>
+	/// Логика взаимодействия для Authorization.xaml
+	/// </summary>
+	public partial class AuthorizationUserControl : UserControl
+	{
+		public AuthorizationUserControl()
+		{
+			InitializeComponent();
+		}
+
+		#region Private
+		
+		#endregion
+	}
+}
